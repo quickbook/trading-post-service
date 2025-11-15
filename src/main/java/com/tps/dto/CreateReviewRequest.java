8a6b18bf3041b8579248c4ab93ac7743ea71d293
quickbook <subbuvmca@gmail.com>
2025-11-15 09:49:47 +0530

    add bean validation annotations

--- src/main/java/com/tps/dto/CreateReviewRequest.java
+++ src/main/java/com/tps/dto/CreateReviewRequest.java
@@ -1,17 +1,31 @@
 package com.tps.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class CreateReviewRequest {     
+public class CreateReviewRequest {
+	
  
-    @NotBlank(message = "Trading Experience is required")
-    private String tradingExp ;
-    
-    @NotBlank(message = "Rating is required")
-    private String rating;
+	@NotBlank(message = "Trading Experience is required")
+	@Pattern(
+	    regexp = "BEGINNER|INTERMEDIATE|ADVANCED|EXPERT|PROFESSIONAL",
+	    message = "Trading Experience must be one of: BEGINNER, INTERMEDIATE, ADVANCED, EXPERT, PROFESSIONAL"
+	)
+	private String tradingExp;
 
-    @NotBlank(message = "Description is required")
-    private String description;
+
+	@NotBlank(message = "Rating is required")
+	@Pattern(
+	    regexp = "A\\+|A|B|C|D",
+	    message = "Rating must be one of: A+, A, B, C, D"
+	)
+	private String rating;
+
+	@NotBlank(message = "Description is required")
+	@Size(max = 2000, message = "Description must be <= 2000 characters")
+	private String description;
+
 }
